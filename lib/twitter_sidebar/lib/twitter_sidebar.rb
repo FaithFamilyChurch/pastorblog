537cb47ecb9c7294814f7e2a23f226575e6f9ff5
Tim Andrews <musicmunky@gmail.com>
2017-04-16 13:08:32 +0000

    Update with Twitter Sidebar plugin

--- lib/twitter_sidebar/lib/twitter_sidebar.rb
+++ lib/twitter_sidebar/lib/twitter_sidebar.rb
@@ -1,11 +1,28 @@
 class TwitterSidebar < Sidebar
-  display_name 'Twitter Feed'
-  description 'Show a given number of the latest tweets from a twitter feed'
+    display_name 'Twitter Feed'
+    description 'Show a given number of the latest tweets from a twitter feed'
 
-  setting :twitter_user, 'Reuters'
-  setting :tweet_count, 5, label: 'Number of Tweets'
+    setting :title, ""
+    setting :tweet_count, 5, label: 'Number of Tweets'
+    setting :twitter_user, 'Reuters'
 
-  attr_accessor :twitter
+    attr_accessor :twitter
+
+    def tweetbuilder
+        @tweetbuilder ||= TweetBuilder.new
+        @client ||= @tweetbuilder.twitter_client
+    end
+
+
+    def client
+        return @client
+    end
+
+
+    def get_tweetbuilder
+        return @tweetbuilder
+    end
+
 
 end
 
