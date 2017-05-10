7d8c98ac2f81ae62f1b101898961c0abb6221082
Tim Andrews <musicmunky@gmail.com>
2017-05-10 20:01:57 +0000

    Update to twitter sidebar to make buttons optional and number of tweets / height variables

--- lib/twitter_sidebar/lib/twitter_sidebar.rb
+++ lib/twitter_sidebar/lib/twitter_sidebar.rb
@@ -3,27 +3,15 @@ class TwitterSidebar < Sidebar
     display_name 'Twitter Feed'
     description 'Show a given number of the latest tweets from a twitter feed'
 
-    setting :title, ""
-    setting :tweet_count, 5, label: 'Number of Tweets'
     setting :twitter_user, 'Reuters', label: "Twitter Account to Follow"
+    setting :title, "Twitter Feed"
+    setting :tweet_count, 5, label: 'Number of Tweets'
+    setting :twitter_height, '1000', label: 'Widget Height'
+    setting :show_follow_button, true, label: 'Show Follow Button?', input_type: :checkbox, onclick: 'changeVal(this)'
+    setting :show_read_more_button, true, label: 'Show Read More Button?', input_type: :checkbox, onclick: 'changeVal(this)'
 
     attr_accessor :twitter
-=begin
-    def tweetbuilder
-        @tweetbuilder ||= TweetBuilder.new
-        @client ||= @tweetbuilder.twitter_client
-    end
 
-
-    def client
-        return @client
-    end
-
-
-    def get_tweetbuilder
-        return @tweetbuilder
-    end
-=end
 end
 
 SidebarRegistry.register_sidebar TwitterSidebar
