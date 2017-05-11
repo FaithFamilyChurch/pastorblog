3932ca3c818eea5af6290570bd36a68cc57f9e9a
Tim Andrews <musicmunky@gmail.com>
2017-05-11 12:06:42 +0000

    Update to add options to twitter sidebar for showing header and footer

--- lib/twitter_sidebar/lib/twitter_sidebar.rb
+++ lib/twitter_sidebar/lib/twitter_sidebar.rb
@@ -7,8 +7,10 @@ class TwitterSidebar < Sidebar
     setting :title, "Twitter Feed"
     setting :tweet_count, 5, label: 'Number of Tweets'
     setting :twitter_height, '1000', label: 'Widget Height (in px)'
-    setting :show_follow_button, true, label: 'Show Follow Button?', input_type: :checkbox, onclick: 'changeVal(this)'
-    setting :show_read_more_button, true, label: 'Show Read More Button?', input_type: :checkbox, onclick: 'changeVal(this)'
+    setting :show_header, true, label: 'Show Header', input_type: :checkbox, onclick: 'changeVal(this)'
+    setting :show_footer, true, label: 'Show Footer', input_type: :checkbox, onclick: 'changeVal(this)'
+    setting :show_follow_button, true, label: 'Show Follow Button', input_type: :checkbox, onclick: 'changeVal(this)'
+    setting :show_read_more_button, true, label: 'Show Read More Button', input_type: :checkbox, onclick: 'changeVal(this)'
 
     attr_accessor :twitter
 
