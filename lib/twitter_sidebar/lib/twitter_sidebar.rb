fd773396958fe385b53c0a4e07281fbf1e356e93
Tim Andrews <musicmunky@gmail.com>
2017-05-11 11:44:46 +0000

    Update to finalize changes for checkbox in sidebar options and styling for search widget

--- lib/twitter_sidebar/lib/twitter_sidebar.rb
+++ lib/twitter_sidebar/lib/twitter_sidebar.rb
@@ -1,12 +1,12 @@
 class TwitterSidebar < Sidebar
 
     display_name 'Twitter Feed'
-    description 'Show a given number of the latest tweets from a twitter feed'
+    description 'Show the latest tweets from a twitter feed'
 
-    setting :twitter_user, 'Reuters', label: "Twitter Account to Follow"
+    setting :twitter_user, 'Reuters', label: "Twitter Username"
     setting :title, "Twitter Feed"
     setting :tweet_count, 5, label: 'Number of Tweets'
-    setting :twitter_height, '1000', label: 'Widget Height'
+    setting :twitter_height, '1000', label: 'Widget Height (in px)'
     setting :show_follow_button, true, label: 'Show Follow Button?', input_type: :checkbox, onclick: 'changeVal(this)'
     setting :show_read_more_button, true, label: 'Show Read More Button?', input_type: :checkbox, onclick: 'changeVal(this)'
 
