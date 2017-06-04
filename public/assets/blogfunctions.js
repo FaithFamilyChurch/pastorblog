086e8dc25af26cb4f2dd62051c964aeb5ae61f7f
Tim Andrews <musicmunky@gmail.com>
2017-06-04 16:32:53 +0000

    Update with nav bar indicator fixes from Randy

--- public/assets/blogfunctions.js
+++ public/assets/blogfunctions.js
@@ -7,8 +7,24 @@ $( document ).ready(function() {
         });
     });
 
-    var sPath = window.location.pathname.split("/")[1];
-    sPath = FUSION.lib.isBlank(sPath) ? "home" : sPath;
-    FUSION.get.node(sPath + "_indicator").style.backgroundColor = "#483D3F";
+    FUSION.get.node("blog_indicator").style.backgroundColor = "#483D3F";
 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 170) {
+            $('#navbar').addClass('navbar-fixed');
+            $('#navbar-inner').addClass('navbar-inner-fixed');
+            $('.title').css('height', "200px");
+            $('.titlelinks').css('border', "none");
+            $('.nav-indicator').css('margin-top', '0px');
+            $('.titlelink').css('padding', "4px 10px 10px 10px");
+        }
+        if ($(window).scrollTop() < 171) {
+            $('#navbar').removeClass('navbar-fixed');
+            $('#navbar-inner').removeClass('navbar-inner-fixed');
+            $('.title').css('height', "150px");
+            $('.titlelinks').css('border-top', "1px solid #c1b8be");
+            $('.nav-indicator').css('margin-top', '-6px');
+            $('.titlelink').css('padding', "10px");
+        }
+    });
 });
